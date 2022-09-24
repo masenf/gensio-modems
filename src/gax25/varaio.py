f2036037840b81117403c4503cba539b07af1575
Masen Furer <m_github@0x26.net>
2022-09-24 00:42:54 -0700

    vara wip

---
+++ src/gax25/varaio.py
@@ -0,0 +1,78 @@
+import logging
+
+
+logger = logging.getLogger("varaio")
+
+
+class VaraListener:
+    def __init__(self, laddr, data_port, spawn="stdio(self)"):
+        self.laddr = laddr
+        self.data_port = data_port
+        self.spawn = spawn
+        self._io = None  # the vara command side of the connection
+        self.bufIn = array.array("B")
+        self.bufOut = array.array("B", "\r".join([
+			f"MYCALL {laddr}".encode("utf-8"),
+        self.in_close = False  # true if either gensio is down or going down
+
+    @property
+    def io(self):
+        return self._io
+
+    @io.setter
+    def io(self, value):
+        self._io = value
+        if self._io is not None:
+            self._io.set_cbs(self)
+
+    def close(self, io):
+        self.in_close = True
+        self.io.write_cb_enable(True)
+
+    def read_callback(self, io, err, data, auxdata):
+        if err:
+            if err != "Remote end closed connection":
+                self.log_for(io, "read error: %s", err)
+            self.close(io)
+            return 0
+        if data:
+            logger.debug("read_callback: data=%r", data)
+            for command in 
+            self.bufIn.extend(data)
+        if self.bufOut:
+            self.io.write_cb_enable(True)
+        return len(data)
+
+    def write_callback(self, io):
+        buf = self.bufOut
+        if buf:
+            try:
+                count = io.write(buf.tobytes(), None)
+            except Exception as e:
+                if str(e) != "Remote end closed connection":
+                    logger.error("write error: %s", e)
+                self.close(io)
+                return
+            logger.debug(io, "write_callback: data=%s", buf[:count].tobytes())
+            buf[:] = buf[count:]
+
+        if not buf:
+            io.write_cb_enable(False)
+            if self.in_close:
+                io.close(self)
+
+    def open_done(self, io, err):
+        if err:
+            logger.error("open error: %s", err)
+            io.close(self)
+            if self.io is not None:
+                self.io.close(self)
+            return
+        logger.info(io, "Opened gensio: %s", io)
+        io.write_cb_enable(True)
+        io.read_cb_enable(True)
+
+    def close_done(self, io):
+        self.log_for(io, "closed gensio: %s", io)
+
+
